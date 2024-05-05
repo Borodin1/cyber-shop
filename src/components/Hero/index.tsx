fbe5b8665a04ac09d4d753798afb0f88566aaa02
Влад Варламов <vladvarlamov@MacBook-Pro-Vlad.local>
2024-05-05 23:31:34 +0300

    compleate hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -13,11 +13,13 @@ export default function Hero({ }: IHeroProps) {
             <div className={`${styles.container}`}>
                 <div className={styles.info}>
                     <h2 className={styles.h2}>Pro.Beyond.</h2>
-                    <h1 className={styles.h1}>IPhone 14 Pro</h1>
+                    <h1 className={styles.h1}>IPhone 14 <span>Pro</span></h1>
                     <p className={styles.p}>Created to change everything for the better. For everyone</p>
                     <Button variant={'ghost'} size={'lg'}>Shop Now</Button>
                 </div>
-                <Image src={'/iphone.png'} alt="iphone" width={406} height={632} />
+                <div className="">
+                    <Image src={'/iphone.png'} alt="iphone" width={406} height={632} className="max-w-full" />
+                </div>
             </div>
         </div>
     )
