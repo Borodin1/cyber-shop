b6f669c34347daf922da47d46125f0ed2d554a0f
Alexander Borodin <98040616+Borodin1@users.noreply.github.com>
2024-05-09 22:05:42 +0300

    feat:add order summary to CartPage

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,13 +1,28 @@
 /* eslint-disable react/jsx-key */
+import styles from './Cart.module.scss'
 import CartItems from "~/components/CartItems";
 import { arr } from "./arr";
+import CartSum from '~/components/CartSum'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "Cart | Cyber Shop",
+};;
 
 export default function CartPage() {
   return (
-    <div>
-      <div>Shopping Cart</div>
-      {arr.map((items)=><CartItems items={items}/>
+    <div className={styles.cart}>
+      <div className={styles.cartTitle}>
+        <h2>Shopping Cart</h2>
+        </div>
+      <div className={styles.cartContent}>
+        <div>
+      {arr.map((items)=>
+      <CartItems items={items}/>
       )}
+      </div>
+      <CartSum/>
+      </div>
     </div>
   );
 }
